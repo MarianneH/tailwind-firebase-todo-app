d9f03a6192ba1538ea0047b1a52ba2c44096fda9
MarianneH1 <marianne.helbig@googlemail.com>
2023-03-13 15:50:32 +0100

    refactor Button component

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Button({ children }) {
+function Button({ children }: { children: String }) {
   return (
     <button className="border-none text-white rounded-lg mt-2 px-4 py-2 bg-violet-500 hover:bg-violet-600 hover:drop-shadow-md">
       {children}
