b810a0ee140771117e0483ce8cee96d17128f929
MarianneH1 <marianne.helbig@googlemail.com>
2023-03-30 14:05:35 +0200

    add select elem as time select for time estimate

--- src/component/TodoModal/TodoDetails.tsx
+++ src/component/TodoModal/TodoDetails.tsx
@@ -1,6 +1,5 @@
 import { TodoProps } from "@/types/TodoProps";
 import React, { Dispatch, SetStateAction } from "react";
-import InputField from "../InputField";
 import EditTodoTitle from "./EditTodoTitle";
 
 function TodoDetails({
@@ -27,7 +26,6 @@ function TodoDetails({
               />
             </td>
           </tr>
-
           <tr>
             <td>urgent:</td>
             <input type="checkbox" id="urgent" name="urgent" className="ml-4" />
@@ -47,7 +45,17 @@ function TodoDetails({
           </tr>
           <tr>
             <td>time estimate:</td>
-            <input type="text" name="estimate" id="estimate" className="ml-4" />
+            <select name="time_estimate" id="time_estimate" className="ml-4">
+              <option value={undefined}>-</option>
+              <option value={0.25}>15 mins</option>
+              <option value={0.5}>30 mins</option>
+              <option value={0.75}>45 mins</option>
+              <option value={1}>1 hour</option>
+              <option value={2}>2 hour</option>
+              <option value={3}>3 hour</option>
+              <option value={4}>1/2 day</option>
+              <option value={8}>1 day</option>
+            </select>
           </tr>
         </tbody>
       </table>
