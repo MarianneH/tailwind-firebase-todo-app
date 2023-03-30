93f553a419c7e640fc5c7e1e61d8ec82fbf7537b
MarianneH1 <marianne.helbig@googlemail.com>
2023-03-30 14:57:41 +0200

    change modal header title

--- src/component/TodoModal/TodoModal.tsx
+++ src/component/TodoModal/TodoModal.tsx
@@ -26,7 +26,9 @@ function TodoModal({
     <div className="flex items-start justify-center fixed top-0 left-0 h-screen w-full overflow-y-scroll z-10 bg-opacity-25 bg-black">
       <div className="bg-white dark:bg-gray-800 shadow-md p-6 w-11/12 max-w-3xl text-left mx-auto my-12">
         <div className="flex justify-between text-lg mb-4">
-          <div className="text-opacity-60 font-bold uppercase">In focus</div>
+          <div className="text-opacity-60 font-bold uppercase">
+            Update this todo
+          </div>
           <div onClick={closeModalClick} className="cursor-pointer">
             &#10005;
           </div>
